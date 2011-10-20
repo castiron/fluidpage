94c9b88e7b3dd151ccd4e242862b0a77d39d8694
Zach Davis <zach@castironcoding.com>
2011-10-20 11:05:29 -0700

    [FEATURE] Removed the body class field from backend layouts; this can be handled with typoscript

--- ext_tables.php
+++ ext_tables.php
@@ -7,20 +7,5 @@ t3lib_extMgm::addStaticFile($_EXTKEY,'static/fluid_page_static_typoscript_templa
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/fluid_page_sample_page_templates/', 'Fluid Page Sample Page Templates');
 t3lib_extMgm::addPlugin(array('LLL:EXT:'.$_EXTKEY.'/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
 
-$tempColumns = array (
-    'tx_fluidpage_body_class' => array (        
-        'exclude' => 0,        
-        'label' => 'LLL:EXT:fluidpage/locallang_db.xml:backend_layout.tx_fluidpage_body_class',        
-        'config' => array (
-            'type' => 'input',    
-            'size' => '30',
-        )
-    ),
-);
-
-
-t3lib_div::loadTCA('backend_layout');
-t3lib_extMgm::addTCAcolumns('backend_layout',$tempColumns,1);
-t3lib_extMgm::addToAllTCAtypes('backend_layout','tx_fluidpage_body_class;;;;1-1-1');
 
 ?>
--- ext_tables.sql
+++
@@ -1,6 +0,0 @@
-#
-# Table structure for table 'backend_layout'
-#
-CREATE TABLE backend_layout (
-	tx_fluidpage_body_class tinytext
-);
