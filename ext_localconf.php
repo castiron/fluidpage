94ddb31f1e294c357df33d9099a141edaee2d621
Zach Davis <zach@castironcoding.com>
2011-10-20 13:48:07 -0700

    [FEATURE] Added rootline field config to ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,4 +3,6 @@
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_fluidpage_pi1.php','_pi1','list_type',1);
 t3lib_extMgm::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fluidpage/static/tsconfig/pageTSConfig.txt">');
 
+$GLOBALS["TYPO3_CONF_VARS"]["FE"]["addRootLineFields"] .= 'backend_layout,backend_layout_next_level';
+
 ?>
