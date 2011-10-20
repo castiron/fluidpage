aaa6b627483ce6f844fadf15bf9c77ed6600802b
Zach Davis <zach@castironcoding.com>
2011-10-20 13:49:12 -0700

    [BUGFIX] Removed body class field that FluidPage was adding to backend layout reocrds

--- ext_tables.php
+++ ext_tables.php
@@ -4,8 +4,6 @@ if (!defined('TYPO3_MODE')) {
 }
 
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/fluid_page_static_typoscript_template/', 'Fluid Page Static Typoscript Template');
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/fluid_page_sample_page_templates/', 'Fluid Page Sample Page Templates');
 t3lib_extMgm::addPlugin(array('LLL:EXT:'.$_EXTKEY.'/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
-
 
 ?>
