c6974c1c3a6eed69f308e126252ece9fe1a1a7b1
Michael McManus <michaelorionmcmanus@gmail.com>
2011-10-27 16:37:46 -0700

    [FEATURE] Process of adding backend_layout page fields to the front end rootline is more robust.

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,10 @@ t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_fluidpage_pi1.php','_pi1','list
 
 t3lib_extMgm::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fluidpage/static/tsconfig/pageTSConfig.txt">');
 
-
-$GLOBALS["TYPO3_CONF_VARS"]["FE"]["addRootLineFields"] .= 'backend_layout,backend_layout_next_level';
+if(!empty($GLOBALS["TYPO3_CONF_VARS"]["FE"]["addRootLineFields"])) {
+	$GLOBALS["TYPO3_CONF_VARS"]["FE"]["addRootLineFields"] .= ','."backend_layout,backend_layout_next_level";
+} else {
+	$GLOBALS["TYPO3_CONF_VARS"]["FE"]["addRootLineFields"] .= "backend_layout,backend_layout_next_level";
+}
 
 ?>
