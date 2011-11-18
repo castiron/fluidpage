8fcc92cf43534744f039973cac0c1d2a6a954039
Zach Davis <zach@castironcoding.com>
2011-11-18 11:42:22 -0800

    [CHORE] Released 0.2.0 to TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "fluidpage".
 #
-# Auto generated 20-10-2011 14:09
+# Auto generated 15-11-2011 10:26
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.0',
+	'version' => '0.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'fluid' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:35:{s:9:"ChangeLog";s:4:"7c38";s:10:"README.txt";s:4:"12b2";s:16:"ext_autoload.php";s:4:"e35f";s:12:"ext_icon.gif";s:4:"8343";s:17:"ext_localconf.php";s:4:"18d8";s:14:"ext_tables.php";s:4:"fb7a";s:30:"Resources/Public/css/style.css";s:4:"1534";s:31:"Resources/Public/html/main.html";s:4:"4b7c";s:30:"Resources/Public/images/bg.gif";s:4:"20be";s:42:"Resources/Public/images/bg_contentWrap.gif";s:4:"9edc";s:41:"Resources/Public/images/bg_firstLevel.gif";s:4:"1186";s:37:"Resources/Public/images/bg_footer.jpg";s:4:"1826";s:41:"Resources/Public/images/bg_getStarted.jpg";s:4:"8415";s:37:"Resources/Public/images/bg_header.jpg";s:4:"b45b";s:40:"Resources/Public/images/bg_liCurrent.gif";s:4:"d6cd";s:43:"Resources/Public/images/bg_searchButton.gif";s:4:"a13d";s:42:"Resources/Public/images/bg_searchInput.gif";s:4:"0fdf";s:45:"Resources/Public/images/bg_secondaryLevel.jpg";s:4:"62ad";s:47:"Resources/Public/images/button_logintotypo3.png";s:4:"69bf";s:40:"Resources/Public/images/flower_mask2.png";s:4:"2c56";s:42:"Resources/Public/images/header_overlay.png";s:4:"501b";s:44:"Resources/Public/images/navButtonOverlay.png";s:4:"4d85";s:38:"Resources/Public/images/navOverlay.png";s:4:"73f1";s:33:"Resources/Public/images/step1.png";s:4:"748b";s:33:"Resources/Public/images/step2.png";s:4:"7317";s:33:"Resources/Public/images/step3.png";s:4:"4070";s:13:"doc/howto.rst";s:4:"55e0";s:14:"doc/styles.css";s:4:"1adc";s:20:"doc/build/index.html";s:4:"d348";s:30:"pi1/class.tx_fluidpage_pi1.php";s:4:"4908";s:53:"static/fluid_page_sample_page_templates/constants.txt";s:4:"f6fe";s:49:"static/fluid_page_sample_page_templates/setup.txt";s:4:"dabd";s:58:"static/fluid_page_static_typoscript_template/constants.txt";s:4:"9363";s:54:"static/fluid_page_static_typoscript_template/setup.txt";s:4:"660f";s:32:"static/tsconfig/pageTSConfig.txt";s:4:"84af";}',
+	'_md5_values_when_last_written' => 'a:39:{s:9:"ChangeLog";s:4:"7c38";s:10:"README.txt";s:4:"12b2";s:16:"ext_autoload.php";s:4:"109f";s:12:"ext_icon.gif";s:4:"8343";s:17:"ext_localconf.php";s:4:"955f";s:14:"ext_tables.php";s:4:"fb7a";s:31:"Classes/Controller/Template.php";s:4:"f34a";s:26:"Classes/Model/Template.php";s:4:"0ef2";s:35:"Classes/Service/TemplateFactory.php";s:4:"1f52";s:41:"Classes/ViewHelpers/ContentViewHelper.php";s:4:"5a5f";s:30:"Resources/Public/css/style.css";s:4:"a763";s:31:"Resources/Public/html/main.html";s:4:"3909";s:30:"Resources/Public/images/bg.gif";s:4:"20be";s:42:"Resources/Public/images/bg_contentWrap.gif";s:4:"9edc";s:41:"Resources/Public/images/bg_firstLevel.gif";s:4:"1186";s:37:"Resources/Public/images/bg_footer.jpg";s:4:"1826";s:41:"Resources/Public/images/bg_getStarted.jpg";s:4:"8415";s:37:"Resources/Public/images/bg_header.jpg";s:4:"b45b";s:40:"Resources/Public/images/bg_liCurrent.gif";s:4:"d6cd";s:43:"Resources/Public/images/bg_searchButton.gif";s:4:"a13d";s:42:"Resources/Public/images/bg_searchInput.gif";s:4:"0fdf";s:45:"Resources/Public/images/bg_secondaryLevel.jpg";s:4:"62ad";s:47:"Resources/Public/images/button_logintotypo3.png";s:4:"69bf";s:40:"Resources/Public/images/flower_mask2.png";s:4:"2c56";s:42:"Resources/Public/images/header_overlay.png";s:4:"501b";s:44:"Resources/Public/images/navButtonOverlay.png";s:4:"4d85";s:38:"Resources/Public/images/navOverlay.png";s:4:"73f1";s:33:"Resources/Public/images/step1.png";s:4:"748b";s:33:"Resources/Public/images/step2.png";s:4:"7317";s:33:"Resources/Public/images/step3.png";s:4:"4070";s:13:"doc/howto.rst";s:4:"edf0";s:14:"doc/styles.css";s:4:"1adc";s:20:"doc/build/index.html";s:4:"280c";s:30:"pi1/class.tx_fluidpage_pi1.php";s:4:"852e";s:53:"static/fluid_page_sample_page_templates/constants.txt";s:4:"f6fe";s:49:"static/fluid_page_sample_page_templates/setup.txt";s:4:"dabd";s:58:"static/fluid_page_static_typoscript_template/constants.txt";s:4:"9363";s:54:"static/fluid_page_static_typoscript_template/setup.txt";s:4:"660f";s:32:"static/tsconfig/pageTSConfig.txt";s:4:"84af";}',
 	'suggests' => array(
 	),
 );
